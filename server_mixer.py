b8a9bf4c0bfdda3cb955415df8995858abfec067
cleiven <91392513+cleiven@users.noreply.github.com>
2024-12-26 23:01:21 -0500

    Update server_mixer.py

--- server_mixer.py
+++ server_mixer.py
@@ -7,5 +7,20 @@ from serverlib import *
 # - port
 # - world map (get some custom maps?)
 # - datapack (put a bunch of random commands in it lmfao, also world settings)
+
+
+class Settings:
+    server_path = "server_mixer"
+    random_port = True
+    random_version = True
+    random_world = True
+    random_datapack = True
+    
 def main():
-     
+     Vanilla.create_server(Settings.server_path,"random")
+     with open(f"server_path/server.properties","r+") as f:
+        server_properties = f.read()
+        # make changes etc
+        f.write(server_properties)
+if __name__ == "__main__":
+    main()
